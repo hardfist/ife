a52c4f35c0bccad2747055e001f08c9b67d1f40c
shengju.yj <shengju.yj@alibaba-inc.com>
2016-07-19 17:24:43 +0800

    树形组件重构

--- task25/index.js
+++ task25/index.js
@@ -1,26 +1,50 @@
 var app = angular.module('app',[]);
-app.controller('MainCtrl',function($scope){
-    $scope.model = {
-        value: 'Super',
-        children:[
-            {
-                value: 'CellPhone1',
-                children:[
-                    {value: 'Pear1'},
-                    {value: 'Pig1'},
-                    {value: 'Test1'}
-                ]
-            },
-            {
-                value: 'CellPhone2',
-                children:[
-                    {value: 'Pear2'},
-                    {value: 'Pig2'},
-                    {value: 'Test2'}
-                ]
+class Tree{
+    constructor(value,children){
+        this.value = value;
+        this.parent = null;
+        this.children = [];
+        if(children && children.length >0) {
+            for (let child of children) {
+                this.addChild(child);
             }
-        ]
+        }
     }
+    addChild(node){
+        this.children.push(node);
+        node.parent = this;
+        return this;
+    }
+    deleteChild(node){
+        node.parent = null;
+        let index = this.children.indexOf(node);
+        if(index!=-1){
+            this.children.splice(index,1);
+        }
+        return this;
+    }
+    deleteSelf(){
+        let parent = this.parent;
+        if(parent){
+            parent.deleteChild(this);
+        }
+        return this;
+    }
+}
+var model = new Tree('Super',[
+    new Tree('CellPhone1',[
+        new Tree('Pear1'),
+        new Tree('Pear2'),
+        new Tree('Pear3')
+    ]),
+    new Tree('CellPhone2',[
+        new Tree('Pear2'),
+        new Tree('Pig2'),
+        new Tree('Test2')
+    ])
+])
+app.controller('MainCtrl',function($scope){
+    $scope.model = model;
 });
 app.controller('treeCtrl',function($scope){
     $scope.isFolder = function(){
@@ -30,13 +54,21 @@ app.controller('treeCtrl',function($scope){
         return $scope.open = !$scope.open;
     };
     $scope.addItem = function(){
-        var text = prompt('请输入节点内容');
-        $scope.model.children = $scope.model.children || [];
-        $scope.model.children.push({
-            value: text
-        })
+        let text = prompt('请输入节点内容');
+        let newNode = new Tree(text);
+        $scope.model.addChild(newNode);
+    };
+    $scope.deleteItem = function(){
+        $scope.model.deleteSelf();
+    };
+    $scope.showTip = function(){
+        $scope.show = true;
+    };
+    $scope.hideTip = function(){
+        $scope.show = false;
     };
     $scope.open = false;
+    $scope.show = false;
 });
 app.directive('treeDirective',function(){
     return{
